eabc0fa6818894265c3c73d98b048235480d0a0d
Prajna Sariputra <putr4.s@gmail.com>
2019-04-11 15:00:20 +1000

    Add the Activity struct and conversion to "HTML" table
    Output is actually meant for graphviz, but for now this is only the
    label for an Activity

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
+#[macro_use]
 extern crate horrorshow;
-use horrorshow::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+	let act = Activity {id: "1".to_string(), desc: "Desc".to_string(), dur: 2, pred: Vec::new()};
+    println!("{}", act.get_output());
+}
+
+struct Activity {
+	id: String,
+	desc: String, //description
+	dur: u32, //duration
+	pred: Vec<String>, //predecessors
+}
+
+impl Activity {
+	fn get_output(&self) -> String {
+		format!("{}", html!(
+			table(border="0", cellborder="1", cellspacing="0") {
+				tr {
+					td: "ES";
+					td: self.id.clone();
+					td: "EF";
+				}
+				tr {
+					td: "SL";
+					td(colspan="2"): self.desc.clone();
+				}
+				tr {
+					td: "LS";
+					td: self.dur;
+					td: "LF";
+				}
+			}
+		))
+	}
 }
