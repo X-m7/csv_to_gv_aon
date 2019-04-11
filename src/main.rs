2c2d9e11067d80f5a89af6a816e45d5db740f82e
Prajna Sariputra <putr4.s@gmail.com>
2019-04-11 17:13:19 +1000

    Implemented CSV reading
    Expected syntax: 1st line ignored (assumed heading), no commas in any
    fields, multiple predecessors separated by semicolon

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #[macro_use]
 extern crate horrorshow;
 
+use std::env;
+use std::fs::File;
+
+extern crate file_scanner;
+use file_scanner::Scanner;
+
 const GV_BEGIN: &str = "
 digraph aon {
 rankdir=LR;
@@ -9,9 +15,55 @@ node [shape = plain];
 ";
 
 fn main() {
-	let act = Activity {id: "1".to_string(), desc: "Desc".to_string(), dur: 2, pred: Vec::new()};
-	let act2 = Activity {id: "2".to_string(), desc: "Desc".to_string(), dur: 2, pred: vec!["1".to_string()]};
-    println!("{}", gen_gv(vec![act, act2]));
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+        panic!("Input file name required as argument");
+    }
+
+	let activities = get_activities_from_csv(args[1].to_string());
+    println!("{}", gen_gv(activities));
+}
+
+fn get_activities_from_csv(input_filename: String) -> Vec<Activity> {
+	let file = File::open(input_filename);
+	if file.is_err() {
+		panic!("Failed to read file");
+	}
+	let mut file = Scanner::new(file.unwrap());
+	file.next_line(); //skip the first line, assumed to be column titles
+	let mut activities: Vec<Activity> = Vec::new();
+	let mut activity = Activity {id: "".to_string(), desc: "".to_string(), dur: 0, pred: Vec::new()};
+	while let Some(input) = file.next_line() {
+		let mut counter = 0;
+		for i in input.split(',') {
+			match counter {
+				0 => {
+					activity.id = i.to_string();
+					counter += 1;
+				},
+				1 => {
+					activity.desc = i.to_string();
+					counter += 1;
+				},
+				2 => {
+					activity.dur = i.to_string().parse::<u32>().unwrap();
+					counter += 1;
+				},
+				3 => {
+					if !i.is_empty() {
+						for j in i.to_string().split(';') {
+							activity.pred.push(j.to_string());
+						}
+					}
+					activities.push(activity);
+					activity = Activity {id: "".to_string(), desc: "".to_string(), dur: 0, pred: Vec::new()};
+					counter = 0;
+				},
+				_ => ()
+			}
+		}
+	}
+	activities
 }
 
 struct Activity {
