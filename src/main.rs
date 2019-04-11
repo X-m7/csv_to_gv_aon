207b7119caa8b413959fbbd06028877db4be9dc3
Prajna Sariputra <putr4.s@gmail.com>
2019-04-12 07:02:05 +1000

    Store data of each Activity's successors in addition to predecessors
    Actually stored in the ActivityStats struct to avoid ownership issues
    with the Activity struct, also it is only needed when calculating the
    statistics anyway

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate horrorshow;
 
 use std::env;
 use std::fs::File;
+use std::collections::HashMap;
 
 extern crate csv;
 
@@ -20,30 +21,48 @@ fn main() {
     }
 
 	let activities = get_activities_from_csv(args[1].to_string());
-    println!("{}", gen_gv(activities));
+    println!("{}", gen_gv(activities.0));
 }
 
-fn get_activities_from_csv(input_filename: String) -> Vec<Activity> {
+fn get_activities_from_csv(input_filename: String) -> (Vec<Activity>, HashMap<String, ActivityStats>) {
 	let file = File::open(input_filename);
 	if file.is_err() {
 		panic!("Failed to read file");
 	}
 	let mut rdr = csv::Reader::from_reader(file.unwrap());
 	let mut activities: Vec<Activity> = Vec::new();
+	let mut act_stats: HashMap<String, ActivityStats> = HashMap::new();
 	for result in rdr.records() {
 		let record = result.unwrap();
-		activities.push(Activity {id: record.get(0).unwrap().to_string(), desc: record.get(1).unwrap().to_string(), dur: record.get(2).unwrap().to_string().parse::<u32>().unwrap(), pred: {
+		let activity = Activity {id: {
+			let id = record.get(0).unwrap().to_string();
+			act_stats.insert(id.clone(), ActivityStats {early_start: 0, late_start: 0, early_finish: 0, late_finish: 0, slack: 0, next: Vec::new()});
+			id
+		}, desc: record.get(1).unwrap().to_string(), dur: record.get(2).unwrap().to_string().parse::<u32>().unwrap(), pred: {
 			let mut vec: Vec<String> = Vec::new();
 			let preds = record.get(3).unwrap().to_string();
+			let id = record.get(0).unwrap().to_string();
 			if !preds.is_empty() {
 				for i in preds.split(';') {
-					vec.push(i.to_string())
+					let i_str = i.to_string();
+					act_stats.get_mut(&i_str).unwrap().next.push(id.clone());
+					vec.push(i_str);
 				}
 			}
 			vec
-		}});
+		}};
+		activities.push(activity);
 	}
-	activities
+	(activities, act_stats)
+}
+
+struct ActivityStats {
+	early_start: u32,
+	late_start: u32,
+	early_finish: u32,
+	late_finish: u32,
+	slack: u32,
+	next: Vec<String> //successors of the activity (kept here to avoid ownership issues with the main Activity struct, also only needed when calculating stats anyway)
 }
 
 struct Activity {
