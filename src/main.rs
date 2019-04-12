d256d510ca3ea4782a986fdccf71883e9057377a
Prajna Sariputra <putr4.s@gmail.com>
2019-04-12 10:23:41 +1000

    Documentation and formatting improvements

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #[macro_use]
 extern crate horrorshow;
 
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
-use std::collections::HashMap;
 
 extern crate csv;
 
+///The beginning part of the graphviz (dot format) output
 const GV_BEGIN: &str = "
 digraph aon {
 rankdir=LR;
@@ -15,159 +16,212 @@ node [shape = plain];
 ";
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	if args.len() < 2 {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
         panic!("Input file name required as argument");
     }
 
-	let activities = get_activities_from_csv(args[1].to_string());
+    let activities = get_activities_from_csv(args[1].to_string());
     println!("{}", gen_gv(calc_stats(activities)));
 }
 
-fn get_activities_from_csv(input_filename: String) -> (Vec<Activity>, HashMap<String, ActivityStats>) {
-	let file = File::open(input_filename);
-	if file.is_err() {
-		panic!("Failed to read file");
-	}
-	let mut rdr = csv::Reader::from_reader(file.unwrap());
-	let mut activities: Vec<Activity> = Vec::new();
-	let mut act_stats: HashMap<String, ActivityStats> = HashMap::new();
-	for result in rdr.records() {
-		let record = result.unwrap();
-		let activity = Activity {id: {
-			let id = record.get(0).unwrap().to_string();
-			act_stats.insert(id.clone(), ActivityStats {early_start: 0, late_start: 0, early_finish: 0, late_finish: 0, slack: 0, next: Vec::new()});
-			id
-		}, desc: record.get(1).unwrap().to_string(), dur: record.get(2).unwrap().to_string().parse::<u32>().unwrap(), pred: {
-			let mut vec: Vec<String> = Vec::new();
-			let preds = record.get(3).unwrap().to_string();
-			let id = record.get(0).unwrap().to_string();
-			if !preds.is_empty() {
-				for i in preds.split(';') {
-					let i_str = i.to_string();
-					act_stats.get_mut(&i_str).unwrap().next.push(id.clone());
-					vec.push(i_str);
-				}
-			}
-			vec
-		}};
-		activities.push(activity);
-	}
-	(activities, act_stats)
+/// Expected input file format:
+/// - CSV file
+/// - First row as headers
+/// - Actual data from second row onwards with 4 columns (activity ID, description, duration, predecessors, in that order)
+/// - Predecessors as a string of activity IDs separated by semicolon
+/// - Activities are listed in such a way that the predecessors of each activity are located above it in the file
+fn get_activities_from_csv(
+    input_filename: String,
+) -> (Vec<Activity>, HashMap<String, ActivityStats>) {
+    let file = File::open(input_filename);
+    if file.is_err() {
+        panic!("Failed to read file");
+    }
+    let mut rdr = csv::Reader::from_reader(file.unwrap());
+    let mut activities: Vec<Activity> = Vec::new();
+    let mut act_stats: HashMap<String, ActivityStats> = HashMap::new();
+    for result in rdr.records() {
+        //for each row of the CSV
+        let record = result.unwrap();
+        let activity = Activity {
+            id: {
+                let id = record.get(0).unwrap().to_string();
+                act_stats.insert(
+                    id.clone(),
+                    ActivityStats {
+                        early_start: 0,
+                        late_start: 0,
+                        early_finish: 0,
+                        late_finish: 0,
+                        slack: 0,
+                        next: Vec::new(),
+                    },
+                );
+                id
+            },
+            desc: record.get(1).unwrap().to_string(),
+            dur: record.get(2).unwrap().to_string().parse::<u32>().unwrap(),
+            pred: {
+                let mut vec: Vec<String> = Vec::new();
+                let preds = record.get(3).unwrap().to_string();
+                let id = record.get(0).unwrap().to_string();
+                if !preds.is_empty() {
+                    //if it is we will end up with an empty string as a predecessor, need to ignore that
+                    for i in preds.split(';') {
+                        //for each listed predecessor
+                        let i_str = i.to_string();
+                        act_stats.get_mut(&i_str).unwrap().next.push(id.clone()); //store the current Activity being created as the successor of i
+                        vec.push(i_str);
+                    }
+                }
+                vec
+            },
+        };
+        activities.push(activity);
+    }
+    (activities, act_stats)
 }
 
-fn calc_stats(acts: (Vec<Activity>, HashMap<String, ActivityStats>)) -> (Vec<Activity>, HashMap<String, ActivityStats>) {
-	let (activities, mut act_stats) = acts;
-	for i in &activities { //forward path
-		let mut max_pred_early_finish = 0;
-		for j in &i.pred {
-			let j_stats = &act_stats[&j.clone()];
-			if j_stats.early_finish > max_pred_early_finish {
-				max_pred_early_finish = j_stats.early_finish;
-			}
-		}
-		let mut stats = act_stats.get_mut(&i.id).unwrap();
-		stats.early_start = max_pred_early_finish;
-		stats.early_finish = stats.early_start + i.dur;
-	}
-	for i in &activities {
-		let mut stats = act_stats.get_mut(&i.id).unwrap();
-		if stats.next.is_empty() {
-			stats.late_finish = stats.early_finish;
-		}
-	}
-	for i in activities.iter().rev() {
-		let stats = &act_stats[&i.id];
-		let mut min_next_late_start = u32::max_value();
-		if !stats.next.is_empty() {
-			for j in &stats.next {
-				let j_stats = &act_stats[&j.clone()];
-				if j_stats.late_start < min_next_late_start {
-					min_next_late_start = j_stats.late_start;
-				}
-			}
-		}
-		else {
-			min_next_late_start = stats.early_finish;
-		}
-		let mut stats = act_stats.get_mut(&i.id).unwrap();
-		stats.late_finish = min_next_late_start;
-		stats.late_start = stats.late_finish - i.dur;
-	}
-	for i in &activities {
-		let mut stats = act_stats.get_mut(&i.id).unwrap();
-		stats.slack = stats.late_start - stats.early_start;
-	}
-	(activities, act_stats)
+/// Calculates all the values of the ActivityStats
+fn calc_stats(
+    acts: (Vec<Activity>, HashMap<String, ActivityStats>),
+) -> (Vec<Activity>, HashMap<String, ActivityStats>) {
+    let (activities, mut act_stats) = acts;
+    for i in &activities {
+        //forward pass
+        let mut max_pred_early_finish = 0;
+        for j in &i.pred {
+            //need to find the largest early finish value of the predecessors
+            let j_stats = &act_stats[&j.clone()];
+            if j_stats.early_finish > max_pred_early_finish {
+                max_pred_early_finish = j_stats.early_finish;
+            }
+        }
+        let mut stats = act_stats.get_mut(&i.id).unwrap();
+        stats.early_start = max_pred_early_finish;
+        stats.early_finish = stats.early_start + i.dur;
+    }
+    for i in &activities {
+        //find all activities with no successors
+        let mut stats = act_stats.get_mut(&i.id).unwrap();
+        if stats.next.is_empty() {
+            stats.late_finish = stats.early_finish; //do this as preparation for the backward pass
+        }
+    }
+    for i in activities.iter().rev() {
+        //backward pass
+        let stats = &act_stats[&i.id];
+        let mut min_next_late_start = u32::max_value();
+        if !stats.next.is_empty() {
+            //if there are successors find the minimum late start of them
+            for j in &stats.next {
+                let j_stats = &act_stats[&j.clone()];
+                if j_stats.late_start < min_next_late_start {
+                    min_next_late_start = j_stats.late_start;
+                }
+            }
+        } else {
+            //else (no successors) just use its early finish value
+            min_next_late_start = stats.early_finish;
+        }
+        let mut stats = act_stats.get_mut(&i.id).unwrap();
+        stats.late_finish = min_next_late_start;
+        stats.late_start = stats.late_finish - i.dur;
+    }
+    for i in &activities {
+        //calculate slack for all activities
+        let mut stats = act_stats.get_mut(&i.id).unwrap();
+        stats.slack = stats.late_start - stats.early_start;
+    }
+    (activities, act_stats)
 }
 
+///Stores data for an Activity that will be calculated
 struct ActivityStats {
-	early_start: u32,
-	late_start: u32,
-	early_finish: u32,
-	late_finish: u32,
-	slack: u32,
-	next: Vec<String> //successors of the activity (kept here to avoid ownership issues with the main Activity struct, also only needed when calculating stats anyway)
+    early_start: u32,
+    late_start: u32,
+    early_finish: u32,
+    late_finish: u32,
+    slack: u32,
+    next: Vec<String>, //successors of the activity (kept here to avoid ownership issues with the main Activity struct, also only needed when calculating stats anyway)
 }
 
+///Stores data for an activity that is derived directly from the input file
 struct Activity {
-	id: String,
-	desc: String, //description
-	dur: u32, //duration
-	pred: Vec<String>, //predecessors
+    id: String,
+    desc: String,      //description
+    dur: u32,          //duration
+    pred: Vec<String>, //predecessors
 }
 
 impl Activity {
-	fn get_output(&self, stats: &ActivityStats) -> String {
-		if self.dur != 0 {
-			format!("{}", html!(
-				table(border="0", cellborder="1", cellspacing="0") {
-					tr {
-						td: stats.early_start;
-						td: self.id.clone();
-						td: stats.early_finish;
-					}
-					tr {
-						td: stats.slack;
-						td(colspan="2"): self.desc.clone();
-					}
-					tr {
-						td: stats.late_start;
-						td: self.dur;
-						td: stats.late_finish;
-					}
-				}
-			))
-		}
-		else {
-			format!("{}", html!(
-				table(border="0", cellborder="1", cellspacing="0") {
-					tr {
-						td: self.id.clone();
-					}
-					tr {
-						td: self.desc.clone();
-					}
-				}
-			))
-		}
-	}
+    ///Returns an HTML-style output for use by graphviz (dot format)
+    fn get_output(&self, stats: &ActivityStats) -> String {
+        if self.dur != 0 {
+            //if it is a normal activity
+            format!(
+                "{}",
+                html!(
+                    table(border="0", cellborder="1", cellspacing="0") {
+                        tr {
+                            td: stats.early_start;
+                            td: self.id.clone();
+                            td: stats.early_finish;
+                        }
+                        tr {
+                            td: stats.slack;
+                            td(colspan="2"): self.desc.clone();
+                        }
+                        tr {
+                            td: stats.late_start;
+                            td: self.dur;
+                            td: stats.late_finish;
+                        }
+                    }
+                )
+            )
+        } else {
+            //else assume that it is not meant to be an actual activity (just the project itself, or something else)
+            format!(
+                "{}",
+                html!(
+                    table(border="0", cellborder="1", cellspacing="0") {
+                        tr {
+                            td: self.id.clone();
+                        }
+                        tr {
+                            td: self.desc.clone();
+                        }
+                    }
+                )
+            )
+        }
+    }
 }
 
+///Returns a String for consumption by graphviz (dot format) representing the activities as an Activity-on-Node diagram
 fn gen_gv(acts: (Vec<Activity>, HashMap<String, ActivityStats>)) -> String {
-	let (activities, act_stats) = acts;
-	let mut output = GV_BEGIN.to_string();
-	let mut edges: Vec<(String, String)> = Vec::new();
-	for i in activities {
-		output.push_str(&format!("{} [label = <{}>];\n", i.id, i.get_output(&act_stats[&i.id])));
-		for j in i.pred {
-			edges.push((j, i.id.clone()));
-		}
-	}
-	output.push('\n');
-	for i in edges {
-		output.push_str(&format!("{} -> {}\n", i.0, i.1))
-	}
-	output.push_str("}");
-	output
+    let (activities, act_stats) = acts;
+    let mut output = GV_BEGIN.to_string();
+    let mut edges: Vec<(String, String)> = Vec::new();
+    for i in activities {
+        //create the node definition for each activity
+        output.push_str(&format!(
+            "{} [label = <{}>];\n",
+            i.id,
+            i.get_output(&act_stats[&i.id])
+        ));
+        for j in i.pred {
+            //store the edges to be created later
+            edges.push((j, i.id.clone()));
+        }
+    }
+    output.push('\n'); //newline to separate the edge definitions from the nodes, just to make it easier to read
+    for i in edges {
+        //create all the edges
+        output.push_str(&format!("{} -> {}\n", i.0, i.1))
+    }
+    output.push_str("}"); //the end of the output
+    output
 }
